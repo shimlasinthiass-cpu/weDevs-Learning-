e083da8a3891fcc582d112a9913127b4fe76e8c5
shimlasinthiass-cpu <shimla.sinthia.ss@gmail.com>
2026-01-22 10:59:31 +0600

    Create 404.view.php

---
+++ PHP/Learning/404.view.php
@@ -0,0 +1,19 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+<!-- <?php require('partials/banner.php') ?> -->
+
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <!-- Your content -->
+    <h1 style="color: white;" class="text-2xl font-bold">Sorry, Page Not Found</br></h1>
+    <p>
+        <a = href="/index" style="color: lightblue; text-decoration: underline;">
+            Go Back to Home Page 
+    </p>
+  </div>
+</main>
+
+<?php require('partials/footer.php') ?>
+
+  
+
